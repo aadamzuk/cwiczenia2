0a6317a412ff7112e849b7bdbcc8024e7559a778
aadamzuk <32269564+aadamzuk@users.noreply.github.com>
2017-10-02 17:05:42 +0200

    cwiczenie2

--- cwiczenie1/main.cpp
+++ cwiczenie1/main.cpp
@@ -4,19 +4,31 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n,a=40,b=1;
     cout<<"Podaj liczbe wierszy"<<endl;
     cin>>n;
     for(int j=0;j<n;j++)
     {
-    for(int i=0;i<81;i++)
+    for(int i=0;i<80;i=i+2)
     {
-        if(i>=40-j&&i<=40+j)
-            cout<<"x";
-            else cout<<" ";
+        if(i>=40-b*2)
+        {
+        if(i==a)
+        {
+            for(int k=0;k<b;k++)
+            {
+                cout<<"x ";
+            }
+            b++;
+            a=a-2;
+        }
+        }
+        else cout<<" ";
 
     }
     cout<<endl;
     }
+
+
     return 0;
 }
